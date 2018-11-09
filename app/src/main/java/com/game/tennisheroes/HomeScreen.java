0778e66d1610f7b1851458e8d6920990c481ecd1
Luke <landrws540@gmail.com>
2018-11-09 15:19:02 +0000

    Initial commit. First working version of the Tennis Heroes App

---
+++ app/src/main/java/com/game/tennisheroes/HomeScreen.java
@@ -0,0 +1,26 @@
+package com.game.tennisheroes;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+        ImageButton button1 = findViewById(R.id.imageButton1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.click);
+                mp.start();
+            }
+        });
+
+    }
+    }
+
