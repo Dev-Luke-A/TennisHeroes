13df9277696e00fe152f4165a9b7e9af75207e1a
Luke <landrws540@gmail.com>
2018-11-09 21:15:59 +0000

    3rd Commit: Animation, music

--- app/src/main/java/com/game/tennisheroes/HomeScreen.java
+++ app/src/main/java/com/game/tennisheroes/HomeScreen.java
@@ -5,15 +5,33 @@ import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class HomeScreen extends AppCompatActivity {
-
+     MediaPlayer mp1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        ImageView iv = findViewById(R.id.imageView122);
+        Animation scaleAnimation = new ScaleAnimation(0, 1, 0, 1,Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        scaleAnimation.setDuration(2000);
+        scaleAnimation.setFillAfter(true);
+        scaleAnimation.setInterpolator(new DecelerateInterpolator(1.0f));
+        iv.startAnimation(scaleAnimation);
+
         ImageButton button1 = findViewById(R.id.imageButton1);
+        mp1 = MediaPlayer.create(getApplicationContext(), R.raw.hsmusicfinal);
+        mp1.setLooping(true);
+
+        mp1.start();
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,9 +39,20 @@ public class HomeScreen extends AppCompatActivity {
                 mp.start();
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
 
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+       mp1.stop();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+       mp1.start();
     }
     }
 
