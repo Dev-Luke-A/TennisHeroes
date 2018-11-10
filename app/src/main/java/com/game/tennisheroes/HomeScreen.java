62e2074c8339464bff3042a8128ad06b9085a78c
Luke <landrws540@gmail.com>
2018-11-10 10:07:11 +0000

    3rd Commit: Animation, music

--- app/src/main/java/com/game/tennisheroes/HomeScreen.java
+++ app/src/main/java/com/game/tennisheroes/HomeScreen.java
@@ -2,13 +2,12 @@ package com.game.tennisheroes;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
-import android.view.animation.OvershootInterpolator;
 import android.view.animation.ScaleAnimation;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -19,18 +18,56 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-        ImageView iv = findViewById(R.id.imageView122);
+        int UI_OPTIONS = 0;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                UI_OPTIONS = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+
+            }
+
+        getWindow().getDecorView().setSystemUiVisibility(UI_OPTIONS);
+        final ImageButton iv = findViewById(R.id.imageView122);
         Animation scaleAnimation = new ScaleAnimation(0, 1, 0, 1,Animation.RELATIVE_TO_SELF, 0.5f,
                 Animation.RELATIVE_TO_SELF, 0.5f);
         scaleAnimation.setDuration(2000);
         scaleAnimation.setFillAfter(true);
         scaleAnimation.setInterpolator(new DecelerateInterpolator(1.0f));
-        iv.startAnimation(scaleAnimation);
+         iv.startAnimation(scaleAnimation);
 
         ImageButton button1 = findViewById(R.id.imageButton1);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Animation scaleAnimation1 = new ScaleAnimation(1f, 1.2f, 1f, 1.2f,Animation.RELATIVE_TO_SELF, 0.5f,
+                        Animation.RELATIVE_TO_SELF, 0.5f);
+                scaleAnimation1.setDuration(100);
+                scaleAnimation1.setFillAfter(true);
+                final Animation scaleAnimation2 = new ScaleAnimation(1.2f ,1f, 1.2f, 1,Animation.RELATIVE_TO_SELF, 0.5f,
+                        Animation.RELATIVE_TO_SELF, 0.5f);
+                scaleAnimation2.setDuration(100);
+                scaleAnimation2.setFillAfter(true);
+                iv.startAnimation(scaleAnimation1);
+
+                scaleAnimation1.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        iv.startAnimation(scaleAnimation2);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
+            }
+        });
         mp1 = MediaPlayer.create(getApplicationContext(), R.raw.hsmusicfinal);
         mp1.setLooping(true);
-
         mp1.start();
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/game/tennisheroes/MainActivity.java
+++ app/src/main/java/com/game/tennisheroes/MainActivity.java
@@ -1,6 +1,7 @@
 package com.game.tennisheroes;
 
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int UI_OPTIONS = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            UI_OPTIONS = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        }
+        getWindow().getDecorView().setSystemUiVisibility(UI_OPTIONS);
+
     }
 public void onBackPressed(){
         Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
