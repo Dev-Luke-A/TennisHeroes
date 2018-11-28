b4bf62f0e4f1f5d81a4e4d887388740daf41c4b4
Luke <landrws540@gmail.com>
2018-11-28 18:10:02 +0000

    Commit 13 (revised 2): (smoother) First working version of mainGameActivity

--- app/src/main/java/com/game/tennisheroes/MainGameActivity.java
+++ app/src/main/java/com/game/tennisheroes/MainGameActivity.java
@@ -13,6 +13,7 @@ import android.widget.ImageView;
 public class MainGameActivity extends AppCompatActivity {
    SensorManager sensorManager;
    Sensor accelerometer;
+   public int anim = 0;
    float pos = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,21 +21,31 @@ public class MainGameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_game);
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        final ImageView iv = findViewById(R.id.iv1);
+        final int max = 450;
+        final int min = -450;
         SensorEventListener listener = (new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent sensorEvent) {
 
                 float x = sensorEvent.values[0];
-                int max = 450;
-                int min = -450;
-                if (x * 75 > max) x = max/75;
-                if (x * 75 < min) x = min/75;
-                TranslateAnimation translateAnimation = new TranslateAnimation(pos,x * -75,0,0);
-                translateAnimation.setDuration(100);
-                translateAnimation.setFillAfter(true);
-                final ImageView iv = findViewById(R.id.iv1);
-                iv.startAnimation(translateAnimation);
-                pos = (x * -75);
+                if (x * 125 > max) x = max/125;
+                if (x * 125 < min) x = min/125;
+                float x75 = x * -125;
+                if(Math.abs(x75-pos) < 1000) {
+                    TranslateAnimation translateAnimation = new TranslateAnimation(pos, x75, 0, 0);
+                    translateAnimation.setDuration(90);
+                    pos = x75;
+                    translateAnimation.setFillAfter(true);
+                    iv.startAnimation(translateAnimation);
+                }else{
+                    TranslateAnimation translateAnimation = new TranslateAnimation(pos, x75, 0, 0);
+                    translateAnimation.setDuration(250);
+                    pos = x75;
+                    translateAnimation.setFillAfter(true);
+                    iv.startAnimation(translateAnimation);
+                }
             }
 
 
