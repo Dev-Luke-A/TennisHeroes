76cc18d679a03087fb93b1c3d2d424e198d33e9a
Luke <landrws540@gmail.com>
2018-11-29 07:40:46 +0000

    Commit 14: Much smoother animations Touch stops animation

--- app/src/main/java/com/game/tennisheroes/MainGameActivity.java
+++ app/src/main/java/com/game/tennisheroes/MainGameActivity.java
@@ -7,14 +7,16 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
 public class MainGameActivity extends AppCompatActivity {
-   SensorManager sensorManager;
-   Sensor accelerometer;
-   public int anim = 0;
-   float pos = 0;
+    SensorManager sensorManager;
+    Sensor accelerometer;
+    public int anim = 0;
+    float pos = 0;
+   boolean touch = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,21 +32,25 @@ public class MainGameActivity extends AppCompatActivity {
             public void onSensorChanged(SensorEvent sensorEvent) {
 
                 float x = sensorEvent.values[0];
-                if (x * 125 > max) x = max/125;
-                if (x * 125 < min) x = min/125;
+                if (x * 125 > max) x = max / 125;
+                if (x * 125 < min) x = min / 125;
                 float x75 = x * -125;
-                if(Math.abs(x75-pos) < 1000) {
-                    TranslateAnimation translateAnimation = new TranslateAnimation(pos, x75, 0, 0);
-                    translateAnimation.setDuration(90);
-                    pos = x75;
-                    translateAnimation.setFillAfter(true);
-                    iv.startAnimation(translateAnimation);
-                }else{
-                    TranslateAnimation translateAnimation = new TranslateAnimation(pos, x75, 0, 0);
-                    translateAnimation.setDuration(250);
-                    pos = x75;
-                    translateAnimation.setFillAfter(true);
-                    iv.startAnimation(translateAnimation);
+                if(!touch){
+                if (Math.abs(x75 - pos) > 100) {
+                    if (Math.abs(x75 - pos) < 1000) {
+                        TranslateAnimation translateAnimation = new TranslateAnimation(pos, x75, 0, 0);
+                        translateAnimation.setDuration(90);
+                        pos = x75;
+                        translateAnimation.setFillAfter(true);
+                        iv.startAnimation(translateAnimation);
+                    } else {
+                        TranslateAnimation translateAnimation = new TranslateAnimation(pos, x75, 0, 0);
+                        translateAnimation.setDuration(250);
+                        pos = x75;
+                        translateAnimation.setFillAfter(true);
+                        iv.startAnimation(translateAnimation);
+                    }
+                }
                 }
             }
 
@@ -53,15 +59,27 @@ public class MainGameActivity extends AppCompatActivity {
             public void onAccuracyChanged(Sensor sensor, int i) {
 
             }
+
         });
-        sensorManager.registerListener( listener, accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
 
-
-
+        sensorManager.registerListener(listener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
     }
 
-}
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        int action = e.getActionMasked();
+        if (action == MotionEvent.ACTION_UP) {
+            touch = false;
+            return false;
+        } else {
+            touch = true;
+            return true;
+        }
+    }
+    }
+
+
 
 
 
