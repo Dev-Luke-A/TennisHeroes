b50ff6f605509a13d337e97939c2ae727719882a
Luke <landrws540@gmail.com>
2018-12-08 16:36:40 +0000

    Commit 21 (yay!): shop button opens new activity

--- app/src/main/java/com/game/tennisheroes/MainActivity.java
+++ app/src/main/java/com/game/tennisheroes/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
             UI_OPTIONS = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
         }
         getWindow().getDecorView().setSystemUiVisibility(UI_OPTIONS);
+
+        //Tournament button
         ImageButton b1 = findViewById(R.id.imageButton4);
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,8 +30,18 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), MainGameActivity.class);
                 startActivity(intent);
             }
-        });
 
+
+        });
+        //Shop button
+        ImageButton b2 = findViewById(R.id.imageButton3);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ShopActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 public void onBackPressed(){
     finish();
