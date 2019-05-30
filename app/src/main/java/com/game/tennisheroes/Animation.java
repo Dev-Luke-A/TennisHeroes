b2ebd5e5da38b7fa1ed53e6ede43ef7b1e3b7088
Dev-Luke-A <landrews540@gmail.comj>
2019-05-30 14:43:23 +0100

    Commit 27e

--- app/src/main/java/com/game/tennisheroes/Animation.java
+++ app/src/main/java/com/game/tennisheroes/Animation.java
@@ -10,45 +10,50 @@ import android.widget.ImageView;
 import java.util.Timer;
 
 public class Animation extends AppCompatActivity {
-
+    boolean dr1 = true;
+    boolean dr2 = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
        final ImageView iv = findViewById(R.id.imageViews);
+
         new CountDownTimer(4000, 1000) {
             public void onFinish() {
                 Drawable d = getResources().getDrawable(R.drawable.scntwo);
                 iv.setImageDrawable(d);
-                new CountDownTimer(4000, 1000) {
-                    public void onFinish() {
-                        Drawable d2 = getResources().getDrawable(R.drawable.scnthree);
-                        iv.setImageDrawable(d2);
-                        new CountDownTimer(4000, 1000) {
-                            public void onFinish() {
-                                Intent i = new Intent(getApplicationContext(), HomeScreen.class);
-                                startActivity(i);
-                            }
-
-                            public void onTick(long millisUntilFinished) {
-                                // millisUntilFinished    The amount of time until finished.
-                            }
-                        }.start();
-                    }
-
-                    public void onTick(long millisUntilFinished) {
-                        // millisUntilFinished    The amount of time until finished.
-                    }
-                }.start();
             }
 
             public void onTick(long millisUntilFinished) {
                 // millisUntilFinished    The amount of time until finished.
             }
         }.start();
+        if(!dr1) {
 
+            new CountDownTimer(4000, 1000) {
+                public void onFinish() {
+                    Drawable d2 = getResources().getDrawable(R.drawable.scnthree);
+                    iv.setImageDrawable(d2);
+                    dr2 = false;
+                }
 
+                public void onTick(long millisUntilFinished) {
+                    // millisUntilFinished    The amount of time until finished.
+                }
+            }.start();
+        }
+        if(!dr2) {
+            new CountDownTimer(4000, 1000) {
+                public void onFinish() {
+                    Intent i = new Intent(getApplicationContext(), HomeScreen.class);
+                    startActivity(i);
+                }
 
+                public void onTick(long millisUntilFinished) {
+                    // millisUntilFinished    The amount of time until finished.
+                }
+            }.start();
+        }
 
 
 
