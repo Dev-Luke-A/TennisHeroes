73c0fb4d144dc42b8f050de59daf6172238b907b
Luke <landrws540@gmail.com>
2019-05-30 14:15:32 +0100

    commit:27a shops major epic update!!!!

--- app/src/main/java/com/game/tennisheroes/ShopActivity.java
+++ app/src/main/java/com/game/tennisheroes/ShopActivity.java
@@ -13,5 +13,28 @@ Typeface tf = HomeScreen.tf;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
 
+TextView tv2 = findViewById(R.id.textView2);
+tv2.setTypeface(tf);
+TextView tv3 = findViewById(R.id.textView3);
+tv3.setTypeface(tf);
+TextView tv4 = findViewById(R.id.textView4);
+tv4.setTypeface(tf);
+
+TextView tv6 = findViewById(R.id.textView6);
+tv6.setTypeface(tf);
+TextView tv7 = findViewById(R.id.textView7);
+tv7.setTypeface(tf);
+TextView tv8 = findViewById(R.id.textView8);
+tv8.setTypeface(tf);
+
+ int power;
+  int speed;
+   int luck;
+
+ power = 1;
+  speed = 1;
+   luck = 1;
+
+
     }
 }
