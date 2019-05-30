35fcd6ae29740d54cb8e36de18c83bfdfe8472f9
Luke <landrws540@gmail.com>
2019-05-30 15:16:19 +0100

    commit:27g working intro

--- app/src/main/java/com/game/tennisheroes/Animation.java
+++ app/src/main/java/com/game/tennisheroes/Animation.java
@@ -1,6 +1,7 @@
 package com.game.tennisheroes;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
@@ -12,53 +13,62 @@ import java.util.Timer;
 public class Animation extends AppCompatActivity {
     boolean dr1 = true;
     boolean dr2 = true;
+    CountDownTimer ct;
+    CountDownTimer ct2;
+    CountDownTimer ct3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
        final ImageView iv = findViewById(R.id.imageViews);
-while (1==1) {
-    new CountDownTimer(4000, 1000) {
+
+     ct = new CountDownTimer(4000, 1000) {
         public void onFinish() {
             Drawable d = getResources().getDrawable(R.drawable.scntwo);
             iv.setImageDrawable(d);
             dr1 = false;
+            ct2.start();
         }
 
         public void onTick(long millisUntilFinished) {
             // millisUntilFinished    The amount of time until finished.
         }
-    }.start();
-    if (!dr1) {
+    };
+        ct.start();
 
-        new CountDownTimer(4000, 1000) {
+        ct2 = new CountDownTimer(4000, 1000) {
             public void onFinish() {
                 Drawable d2 = getResources().getDrawable(R.drawable.scnthree);
                 iv.setImageDrawable(d2);
                 dr2 = false;
+                ct3.start();
             }
 
             public void onTick(long millisUntilFinished) {
                 // millisUntilFinished    The amount of time until finished.
             }
-        }.start();
-    }
-    if (!dr2) {
-        new CountDownTimer(4000, 1000) {
+        };
+
+         ct3 = new CountDownTimer(4000, 1000) {
             public void onFinish() {
-                Intent i = new Intent(getApplicationContext(), HomeScreen.class);
-                startActivity(i);
+                SharedPreferences prefs = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
+                SharedPreferences.Editor editor = prefs.edit();
+                boolean animation = false;
+                editor.putBoolean("anim",animation);
+
                 finish();
             }
 
             public void onTick(long millisUntilFinished) {
                 // millisUntilFinished    The amount of time until finished.
             }
-        }.start();
-    }
+        };
+
+
+
 }
 
 
 
     }
-}
+
